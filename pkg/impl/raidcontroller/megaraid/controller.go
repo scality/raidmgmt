77d415a1e406b3f92d2bbf6b10bc30aea5e7f95b
Alex Rodriguez <131964409+ezekiel-alexrod@users.noreply.github.com>
2025-01-16 11:48:46 +0100

    revised logic due to review comments
    Still has TODOs and FIXMEs

--- pkg/impl/raidcontroller/megaraid/controller.go
+++ pkg/impl/raidcontroller/megaraid/controller.go
@@ -2,19 +2,21 @@ package megaraid
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
 
+	"github.com/pkg/errors"
 	"github.com/scality/raidmgmt/domain/entities/raidcontroller"
+	"github.com/scality/raidmgmt/utils"
 )
 
-const patternController string = "/c%s"
+// patternController is the pattern for the controller selector.
+const patternController string = "/c%d"
 
 // controllers returns a list of RAID controllers.
 func (a *Adapter) controllers() ([]*raidcontroller.RAIDController, error) {
-	output, err := a.ShowAll()
+	output, err := a.showAll()
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "failed to get controllers information")
 	}
 
 	raidControllers := make([]*raidcontroller.RAIDController, 0)
@@ -22,49 +24,89 @@ func (a *Adapter) controllers() ([]*raidcontroller.RAIDController, error) {
 	for _, controller := range output.Controllers {
 		// Get the system overview for the controller
 		// This is needed to get the controller ID
-		systemOverview, err := unmarshalToSlice[SystemOverview](
+		systemOverview, err := utils.UnmarshalToSlice[SystemOverview](
 			controller.ResponseData, "System Overview",
 		)
 		if err != nil {
-			return nil, err
+			return nil, errors.Wrapf(
+				err,
+				"failed to get system overview for controller %d",
+				controller.CommandStatus.Controller,
+			)
 		}
 
-		sysOvCtrlID := strconv.Itoa(systemOverview[0].Ctl)
+		metadata := raidcontroller.Metadata{
+			// Take the index 0 because there is only one system overview
+			// for a controller
+			ID: systemOverview[0].Ctl,
+		}
 
-		ctrl, err := a.ControllerByID(sysOvCtrlID)
+		// Get the controller
+		ctrl, err := a.controller(&metadata)
 		if err != nil {
-			return nil, err
+			return nil, errors.Wrapf(err, "failed to get controller %d", metadata.ID)
 		}
 
+		// Append the controller to the list
 		raidControllers = append(raidControllers, ctrl)
 	}
 
 	return raidControllers, nil
 }
 
-// ControllerByID returns a RAID controller by its ID.
-func (a *Adapter) ControllerByID(ctrlID string) (*raidcontroller.RAIDController, error) {
-	responseData, err := a.ShowAllController(ctrlID)
+// controller returns a RAID controller for a given metadata.
+func (a *Adapter) controller(metadata *raidcontroller.Metadata) (
+	*raidcontroller.RAIDController,
+	error,
+) {
+	responseData, err := a.showAllController(metadata.ID)
 	if err != nil {
-		return nil, fmt.Errorf("%w: %w", ErrControllerNotFound, err)
+		return nil, errors.Wrapf(err, "failed to get controller %d", metadata.ID)
 	}
 
 	// Get the basics for the controller
 	// This is needed to get the controller model and serial number
-	basics, err := unmarshalToPointer[Basics](responseData, "Basics")
+	basics, err := utils.UnmarshalToPointer[Basics](responseData, "Basics")
 	if err != nil {
-		return nil, fmt.Errorf("%w: %w", ErrControllerNotFound, err)
+		return nil, errors.Wrap(err, "failed to unmarshal basics")
+	}
+
+	// Get the supported adapter operations for the controller
+	// This is needed to check if the controller supports JBOD
+	supportedAdapterOperations, err := utils.UnmarshalToPointer[SupportedAdapterOperations](
+		responseData, "Supported Adapter Operations",
+	)
+	if err != nil {
+		return nil, errors.Wrap(err,
+			"failed to unmarshal supported adapter operations")
+	}
+
+	isJBODSupported := strings.ToLower(supportedAdapterOperations.SupportJBOD) == "yes"
+
+	isJBODEnabled := false
+
+	if isJBODSupported {
+		// Get the capabilities for the controller
+		// This is needed to check if JBOD is enabled
+		capabilities, err := utils.UnmarshalToPointer[Capabilities](responseData, "Capabilities")
+		if err != nil {
+			return nil, errors.Wrap(err,
+				"failed to unmarshal capabilities")
+		}
+
+		isJBODEnabled = strings.ToLower(capabilities.EnableJBOD) == "yes"
 	}
 
 	return &raidcontroller.RAIDController{
-		Metadata: &raidcontroller.Metadata{
-			ID: ctrlID,
-		},
-		Name:   strings.Trim(basics.Model, " "),
-		Serial: strings.Trim(basics.SerialNumber, " "),
+		Metadata:        metadata,
+		Name:            strings.TrimSpace(basics.Model),
+		Serial:          strings.TrimSpace(basics.SerialNumber),
+		IsJBODSupported: isJBODSupported,
+		IsJBODEnabled:   isJBODEnabled,
 	}, nil
 }
 
+// selectorCtrl returns the selector for a controller.
 func selectorCtrl(m *raidcontroller.Metadata) string {
 	return fmt.Sprintf(patternController, m.ID)
 }
