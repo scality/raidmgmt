5f27f5fb6d3670522996c5e1b7d0b2637029bbce
Alex Rodriguez <131964409+ezekiel-alexrod@users.noreply.github.com>
2025-01-16 11:35:28 +0100

    updated domain entities and port

--- pkg/domain/entities/logicalvolume/enums.go
+++ pkg/domain/entities/logicalvolume/enums.go
@@ -2,9 +2,9 @@ package logicalvolume
 
 type (
 	RAIDLevel   uint8
-	ReadPolicy  uint8
-	WritePolicy uint8
-	IOPolicy    uint8
+	ReadPolicy  string
+	WritePolicy string
+	IOPolicy    string
 	LVStatus    uint8
 )
 
@@ -16,32 +16,18 @@ const (
 )
 
 const (
-	ReadPolicyUnknown ReadPolicy = iota
-	ReadPolicyReadAhead
-	ReadPolicyNoReadAhead
+	ReadPolicyUnknown     ReadPolicy = "unknown"
+	ReadPolicyReadAhead   ReadPolicy = "ra"
+	ReadPolicyNoReadAhead ReadPolicy = "nora"
 
-	ReadAhead   string = "ra"
-	NoReadAhead string = "nra"
-)
+	WritePolicyUnknown         WritePolicy = "unknown"
+	WritePolicyWriteBack       WritePolicy = "wb"
+	WritePolicyWriteThrough    WritePolicy = "wt"
+	WritePolicyAlwaysWriteBack WritePolicy = "awb"
 
-const (
-	WritePolicyUnknown WritePolicy = iota
-	WritePolicyWriteBack
-	WritePolicyWriteThrough
-	WritePolicyAlwaysWriteBack
-
-	WriteBack       string = "wb"
-	WriteThrough    string = "wt"
-	AlwaysWriteBack string = "awb"
-)
-
-const (
-	IOPolicyUnknown IOPolicy = iota
-	IOPolicyDirect
-	IOPolicyCached
-
-	Direct string = "direct"
-	Cached string = "cached"
+	IOPolicyUnknown IOPolicy = "unknown"
+	IOPolicyDirect  IOPolicy = "direct"
+	IOPolicyCached  IOPolicy = "cached"
 )
 
 const (
@@ -52,38 +38,3 @@ const (
 	LVStatusDegraded
 	LVStatusFailed
 )
-
-func (r ReadPolicy) String() string {
-	switch r {
-	case ReadPolicyReadAhead:
-		return ReadAhead
-	case ReadPolicyNoReadAhead:
-		return NoReadAhead
-	default:
-		return ""
-	}
-}
-
-func (w WritePolicy) String() string {
-	switch w {
-	case WritePolicyWriteBack:
-		return WriteBack
-	case WritePolicyWriteThrough:
-		return WriteThrough
-	case WritePolicyAlwaysWriteBack:
-		return AlwaysWriteBack
-	default:
-		return ""
-	}
-}
-
-func (i IOPolicy) String() string {
-	switch i {
-	case IOPolicyDirect:
-		return Direct
-	case IOPolicyCached:
-		return Cached
-	default:
-		return ""
-	}
-}
--- pkg/domain/entities/logicalvolume/methods.go
+++ pkg/domain/entities/logicalvolume/methods.go
@@ -1,25 +1,21 @@
 package logicalvolume
 
 import (
-	"fmt"
+	"github.com/pkg/errors"
 )
 
 // Validate checks if the CacheOptions instance is valid.
 func (co *CacheOptions) Validate() error {
-	if co == nil {
-		return ErrCacheOptionsNil
-	}
-
 	if co.ReadPolicy == ReadPolicyUnknown {
-		return ErrUnknownReadPolicy
+		return errors.New("ReadPolicy is unknown")
 	}
 
 	if co.WritePolicy == WritePolicyUnknown {
-		return ErrUnknownWritePolicy
+		return errors.New("WritePolicy is unknown")
 	}
 
 	if co.IOPolicy == IOPolicyUnknown {
-		return ErrUnknownIOPolicy
+		return errors.New("IOPolicy is unknown")
 	}
 
 	return nil
@@ -28,15 +24,15 @@ func (co *CacheOptions) Validate() error {
 // Validate checks if the LogicalVolumeMeta instance is valid.
 func (m *Metadata) Validate() error {
 	if m == nil {
-		return ErrMetadataNil
+		return errors.New("Metadata is nil")
 	}
 
-	if m.CtrlMetadata == nil {
-		return ErrControllerMetaNil
+	if m.ID == "" {
+		return errors.New("ID is empty")
 	}
 
 	if err := m.CtrlMetadata.Validate(); err != nil {
-		return fmt.Errorf("%s: %w", prefixMetadataErr, err)
+		return errors.Wrap(err, "ControllerMetadata is invalid")
 	}
 
 	return nil
@@ -44,40 +40,70 @@ func (m *Metadata) Validate() error {
 
 // Validate checks if the Request instance is valid.
 func (r *Request) Validate() error {
-
 	if r == nil {
-		return ErrRequestNil
-	}
-
-	if r.CtrlMetadata == nil {
-		return ErrControllerMetaNil
+		return errors.New("Request is nil")
 	}
 
 	if err := r.CtrlMetadata.Validate(); err != nil {
-		return fmt.Errorf("%s: %w", prefixRequestErr, err)
+		return errors.Wrap(err, "ControllerMetadata is invalid")
 	}
 
 	if r.RAIDLevel == RAIDLevelUnknown {
-		return ErrUnknownRAIDLevel
+		return errors.New("RAIDLevel is unknown")
 	}
 
 	if len(r.PDrivesMetadata) == 0 {
-		return ErrEmptyPhysicalDrives
+		return errors.New("PhysicalDrives is empty")
+	}
+
+	if err := r.checkRAIDRequirement(); err != nil {
+		return errors.Wrap(err, "RAID level requirement is not met")
 	}
 
 	for _, pdm := range r.PDrivesMetadata {
 		if err := pdm.Validate(); err != nil {
-			return fmt.Errorf("%s: %w", prefixRequestErr, err)
+			return errors.Wrap(err, "PhysicalDriveMetadata is invalid")
 		}
 	}
 
-	if r.CacheOptions == nil {
-		return ErrCacheOptionsNil
-	}
-
 	if err := r.CacheOptions.Validate(); err != nil {
-		return fmt.Errorf("%s: %w", prefixRequestErr, err)
+		return errors.Wrap(err, "CacheOptions is invalid")
 	}
 
 	return nil
+}
+
+// checkRAIDRequirement checks if the RAID level requirements are met.
+// Only regarding the number of physical drives.
+// The identical disk size requirement cannot be checked here.
+func (r *Request) checkRAIDRequirement() error {
+	if r.RAIDLevel == RAIDLevel1 {
+		if len(r.PDrivesMetadata) < RAID1DiskRequirement {
+			return errors.New("not enough physical drives for RAID 1")
+		}
+	}
+
+	if r.RAIDLevel == RAIDLevel10 {
+		if len(r.PDrivesMetadata) < RAID10DiskRequirement {
+			return errors.New("not enough physical drives for RAID 10")
+		}
+
+		if len(r.PDrivesMetadata)%2 != 0 {
+			return errors.New("odd number of physical drives for RAID 10")
+		}
+	}
+
+	return nil
+}
+
+// ToMetadata returns the Metadata instance of the LogicalVolume.
+func (lv *LogicalVolume) ToMetadata() *Metadata {
+	if lv == nil {
+		return nil
+	}
+
+	return &Metadata{
+		CtrlMetadata: lv.CtrlMetadata,
+		ID:           lv.ID,
+	}
 }
