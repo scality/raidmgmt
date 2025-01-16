cf6beee15de30c3c634a152448313e5a48209592
Alex Rodriguez <131964409+ezekiel-alexrod@users.noreply.github.com>
2025-01-16 11:40:04 +0100

    utils package

---
+++ pkg/utils/json.go
@@ -0,0 +1,121 @@
+package utils
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strings"
+)
+
+var (
+	ErrUnmarshal   = errors.New("unmarshal failed")
+	ErrKeyNotFound = errors.New("key not found")
+)
+
+// UnmarshalToSlice unmarshals a JSON response data to a slice.
+func UnmarshalToSlice[T any](responseData json.RawMessage, key string) ([]T, error) {
+	data, found := searchForKey(responseData, key)
+	if !found {
+		return nil, fmt.Errorf("%w: %s", ErrKeyNotFound, key)
+	}
+
+	var slice []T
+
+	if err := json.Unmarshal(data, &slice); err != nil {
+		return nil, fmt.Errorf("%w: %s", ErrUnmarshal, key)
+	}
+
+	return slice, nil
+}
+
+// UnmashalToPointer unmarshals a JSON response data to a pointer.
+func UnmarshalToPointer[T any](responseData json.RawMessage, key string) (*T, error) {
+	data, found := searchForKey(responseData, key)
+	if !found {
+		return nil, fmt.Errorf("%w: %s", ErrKeyNotFound, key)
+	}
+
+	var t T
+
+	if err := json.Unmarshal(data, &t); err != nil {
+		return nil, fmt.Errorf("%w: %s", ErrUnmarshal, key)
+	}
+
+	return &t, nil
+}
+
+// searchForKey searches for a key in a JSON object, including nested objects.
+// If the key is found, the value is returned as json.RawMessage.
+// The function recurses if the value is an object.
+// If the key is not found, the function returns false.
+// nolint: gocognit
+func searchForKey(data json.RawMessage, targetKey string) (json.RawMessage, bool) {
+	decoder := json.NewDecoder(bytes.NewReader(data))
+
+	// Ensure we start processing as an object
+	token, err := decoder.Token()
+	if err != nil || token != json.Delim('{') {
+		return nil, false
+	}
+
+	for decoder.More() {
+		// Read the key
+		key, ok := getKey(decoder)
+		if !ok {
+			return nil, false
+		}
+
+		// If the key matches, return the corresponding value
+		if key == targetKey {
+			return getValue(decoder)
+		}
+
+		// Check if the value is an object to recurse
+		var rawValue json.RawMessage
+		if err := decoder.Decode(&rawValue); err != nil {
+			return nil, false
+		}
+
+		// Check if it's a nested object and recurse
+		if isJSONObject(rawValue) {
+			// Recursively search for the key in the nested object
+			if result, found := searchForKey(rawValue, targetKey); found {
+				return result, true
+			}
+		}
+	}
+
+	return nil, false
+}
+
+// getValue reads the value from a JSON decoder.
+func getValue(decoder *json.Decoder) (json.RawMessage, bool) {
+	var value json.RawMessage
+	if err := decoder.Decode(&value); err != nil {
+		return nil, false
+	}
+
+	return value, true
+}
+
+// getKey reads the key from a JSON decoder.
+func getKey(decoder *json.Decoder) (string, bool) {
+	keyToken, err := decoder.Token()
+	if err != nil {
+		return "", false
+	}
+
+	key, ok := keyToken.(string)
+	if !ok {
+		return "", false
+	}
+
+	return key, true
+}
+
+// isJSONObject checks if a JSON RawMessage is an object.
+func isJSONObject(data json.RawMessage) bool {
+	trimmed := strings.TrimSpace(string(data))
+	return len(trimmed) > 0 && trimmed[0] == '{'
+}
---
+++ pkg/utils/sizing.go
@@ -0,0 +1,54 @@
+package utils
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+const (
+	GB = 1 << 30
+	TB = 1 << 40
+	PB = 1 << 50
+
+	ErrInvalidSizeFormat = "invalid size format: %s"
+	ErrInvalidSizeUnit   = "invalid size unit: %s"
+)
+
+// mapSize is a map of size units to their respective bytes.
+var mapSize = map[string]uint64{
+	"GB": GB,
+	"TB": TB,
+	"PB": PB,
+}
+
+// ConvertSizeBytes converts a size string to bytes.
+func ConvertSizeBytes(size string) (uint64, error) {
+	splitParts := 2
+
+	sizeSplit := strings.Split(size, " ")
+
+	if len(sizeSplit) != splitParts {
+		return 0, errors.Errorf(ErrInvalidSizeFormat, size)
+	}
+
+	// Replace comma with dot for compatibility with ParseFloat
+	normalized := strings.ReplaceAll(sizeSplit[0], ",", ".")
+
+	// Parse the value
+	value, err := strconv.ParseFloat(normalized, 64)
+	if err != nil {
+		return 0, errors.Wrap(err, "failed to parse size value")
+	}
+
+	sizeUnit := sizeSplit[1]
+
+	unit, ok := mapSize[sizeUnit]
+	if !ok {
+		return 0, errors.Errorf(ErrInvalidSizeUnit, sizeUnit)
+	}
+
+	// Calculate the size in bytes
+	return uint64(value * float64(unit)), nil
+}
