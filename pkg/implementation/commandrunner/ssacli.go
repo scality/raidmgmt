078ea2f6472c41062af78b07ec04969de5e7d9c4
Valentin Daviot <valentin.daviot@scality.com>
2025-03-14 16:34:02 +0100

    [PATCH] Change executable path for ssacli.
    Signed-off-by: Valentin Daviot <valentin.daviot@scality.com>

--- pkg/implementation/commandrunner/ssacli.go
+++ pkg/implementation/commandrunner/ssacli.go
@@ -6,7 +6,7 @@ import (
 	"github.com/pkg/errors"
 )
 
-const SSACLIPath = "/opt/hp/hp_ssacli/bin/hp_ssacli"
+const SSACLIPath = "/opt/smartstorageadmin/ssacli/bin/ssacli"
 
 type SSACLI struct {
 	cliPath string
